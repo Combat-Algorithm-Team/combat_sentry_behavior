2f6f17bf82d0f3fbdcc4ab42cf28417cf97db012
Muzjili <1679139025@qq.com>
2026-04-20 16:45:10 +0800

    Add pub_target_mode BT action plugin

---
+++ plugins/action/pub_target_mode.cpp
@@ -0,0 +1,51 @@
+// Copyright 2026 Jieliang Li
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "combat_sentry_behavior/plugins/action/pub_target_mode.hpp"
+
+namespace combat_sentry_behavior
+{
+
+PublishTargetModeAction::PublishTargetModeAction(
+  const std::string & name, const BT::NodeConfig & config, const BT::RosNodeParams & params)
+: RosTopicPubStatefulActionNode(name, config, params)
+{
+}
+
+BT::PortsList PublishTargetModeAction::providedPorts()
+{
+  return providedBasicPorts({
+    BT::InputPort<uint8_t>("target_mode", 0, "Target mode (0-255)"),
+  });
+}
+
+bool PublishTargetModeAction::setMessage(example_interfaces::msg::UInt8 & msg)
+{
+  uint8_t target_mode = 0;
+  getInput("target_mode", target_mode);
+
+  msg.data = static_cast<uint8_t>(target_mode);
+  return true;
+}
+
+bool PublishTargetModeAction::setHaltMessage(example_interfaces::msg::UInt8 & msg)
+{
+  msg.data = 0;
+  return true;
+}
+
+}  // namespace combat_sentry_behavior
+
+#include "behaviortree_ros2/plugins.hpp"
+CreateRosNodePlugin(combat_sentry_behavior::PublishTargetModeAction, "PublishTargetMode");
