d2cc91cb7c00c5526d3d9ffe608ffce5dba0aedc
Muzjili <1679139025@qq.com>
2026-05-11 21:41:18 +0800

    Add bump status behavior publisher

---
+++ include/combat_sentry_behavior/plugins/action/pub_bump_status.hpp
@@ -0,0 +1,33 @@
+// Copyright 2026 Jieliang Li
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef COMBAT_SENTRY_BEHAVIOR__PLUGINS__ACTION__PUB_BUMP_STATUS_HPP_
+#define COMBAT_SENTRY_BEHAVIOR__PLUGINS__ACTION__PUB_BUMP_STATUS_HPP_
+
+#include "combat_sentry_behavior/plugins/action/pub_uint8_base.hpp"
+
+namespace combat_sentry_behavior
+{
+class PublishBumpStatusAction : public PubUInt8Base
+{
+public:
+  PublishBumpStatusAction(
+    const std::string & name, const BT::NodeConfig & config, const BT::RosNodeParams & params);
+
+  static BT::PortsList providedPorts();
+};
+
+}  // namespace combat_sentry_behavior
+
+#endif  // COMBAT_SENTRY_BEHAVIOR__PLUGINS__ACTION__PUB_BUMP_STATUS_HPP_
---
+++ plugins/action/pub_bump_status.cpp
@@ -0,0 +1,36 @@
+// Copyright 2026 Jieliang Li
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "combat_sentry_behavior/plugins/action/pub_bump_status.hpp"
+
+namespace combat_sentry_behavior
+{
+
+PublishBumpStatusAction::PublishBumpStatusAction(
+  const std::string & name, const BT::NodeConfig & config, const BT::RosNodeParams & params)
+: PubUInt8Base(name, config, params, "bump_status")
+{
+}
+
+BT::PortsList PublishBumpStatusAction::providedPorts()
+{
+  return providedBasicPorts({
+    BT::InputPort<uint8_t>("bump_status", 0, "Bump status (0-255)"),
+  });
+}
+
+}  // namespace combat_sentry_behavior
+
+#include "behaviortree_ros2/plugins.hpp"
+CreateRosNodePlugin(combat_sentry_behavior::PublishBumpStatusAction, "PublishBumpStatus");
